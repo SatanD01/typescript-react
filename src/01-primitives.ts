9e8e4bae6d5a8d9e62b64db54b88db04437f0650
Doston <gameplayer140701@gmail.com>
2022-10-04 14:43:17 +0500

    types in TS

---
+++ src/01-primitives.ts
@@ -0,0 +1,28 @@
+// numbers
+let x = 10;
+let y = 10.5;
+
+// string
+let str1 = 'string'
+let str2 = Symbol('as');
+
+// boolean
+let o = true
+
+// nothing
+let h:undefined = undefined
+let g: null = null
+
+// literal
+const num = 123
+const str3 = 'str'
+
+// universal
+let any: any = 1
+any = 'str'
+any = []
+
+let xx: unknown = 2
+if (typeof xx === 'string') {
+    xx.toUpperCase()
+}
