94d5c7e2c4a7cf3aa5ea246d3e63e845cf89bf79
Doston <gameplayer140701@gmail.com>
2022-10-05 12:30:57 +0500

    Tuples in TS

---
+++ dist/06-tuples.js
@@ -0,0 +1,7 @@
+"use strict";
+const pairs = [['key', 'value'], ['key2', 'value2']];
+// Tuples Нужны для того что бы передавать ограниченное количество данных
+const data = [1, true, 'load'];
+// CSV
+const doc = [];
+doc.push(['Josh', 'Mike', 12, new Date(2022, 2, 2)]);
---
+++ src/06-tuples.ts
@@ -0,0 +1,10 @@
+const pairs: [string, string][] = [['key', 'value'], ['key2', 'value2']]
+// Tuples Нужны для того что бы передавать ограниченное количество данных
+const data: [number, boolean, string] = [1, true, 'load'];
+
+// CSV
+
+const doc: [string, string, number, Date][] = [];
+
+doc.push(['Josh', 'Mike', 12, new Date(2022, 2, 2)])
+
