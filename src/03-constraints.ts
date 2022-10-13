f2cfcf344300dc5548fea13478da9d3d32d3e3df
Doston <gameplayer140701@gmail.com>
2022-10-13 12:35:26 +0500

    constraints in TS

---
+++ src/03-constraints.ts
@@ -0,0 +1,14 @@
+function len<T extends {length: number}>(arg: T): number {
+    return arg.length;
+}
+
+// здесь мы ограничеваем количесто типов у generic c помощю extends
+
+len('12');
+len(['2']);
+len({length: 12});
+// len(123);
+// len(true);
+len({a: 1, length: 1});
+
+// object
